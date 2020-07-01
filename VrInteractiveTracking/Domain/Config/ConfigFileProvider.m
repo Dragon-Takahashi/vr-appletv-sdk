e1d77e67917f3af22569869075e23679f6b388e7
Dragon-Takahashi <kazunari.takahashi@dragon.jp>
2020-06-30 18:05:14 -0700

    [auto] release branch

--- VrInteractiveTracking/Domain/Config/ConfigFileProvider.m
+++ VrInteractiveTracking/Domain/Config/ConfigFileProvider.m
@@ -420,6 +420,14 @@
     }
 }
 
+- (BOOL)hasConfigFileWithIdentity:(NSString *)identity {
+    NSString *fixedIdentity = identity;
+    if (!identity) {
+        fixedIdentity = VR_LIB_DEFAULT_LOCAL_FILE_IDENTITY;
+    }
+    return [_configFileList objectForKey:fixedIdentity];
+}
+
 /**
  ローカル設定ファイルを取得
  
